8f08f18cb70c923a7a5594532ca19ca88fd2b0b6
NymaXx <dulze8246@gmail.com>
2020-12-04 19:02:07 -0500

    Se inicializaron las clases

---
+++ src/view/Main.java
@@ -0,0 +1,28 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main(Main.class.getName());
+	}
+	
+	public void settings() {
+		size(1200, 700);
+	}
+	
+	public void setup() {
+		background(255);
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void keyPressed() {
+		
+	}
+
+}
